f9a1451c6cff0e2d702897b52e8124052a33df3b
Christopher Fleetwood <fleetwoodpersonal@gmail.com>
2021-05-24 23:09:46 +0100

    trying to collect into an Array2 goddamit

--- src/lib.rs
+++ src/lib.rs
@@ -18,18 +18,18 @@ use pyo3::{IntoPy, prelude::{pymodule, PyModule, PyResult, Python}};
 fn rust_dtw(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "dtw")]
     fn dtw_py(
-        s: PyReadonlyArray1<'_, f32>,
-        t: PyReadonlyArray1<'_, f32>,
+        s: PyReadonlyArray1<'_, f64>,
+        t: PyReadonlyArray1<'_, f64>,
         window: i32,
         distance_mode: String
-    ) -> f32{ 
+    ) -> f64{ 
         dtw(s.as_array().view(), t.as_array().view(), &window, select_distance(&distance_mode).unwrap(), &distance_mode)
     }
 
-    pub fn dtw(s: ArrayView1<f32>, t: ArrayView1<f32>, window: &i32, distance_fn: fn(&f32, &f32) -> f32, distance_mode: &String) -> f32 {
+    pub fn dtw(s: ArrayView1<f64>, t: ArrayView1<f64>, window: &i32, distance_fn: fn(&f64, &f64) -> f64, distance_mode: &String) -> f64 {
         let m = s.len() + 1;
         let n = t.len() + 1;
-        let mut dtw = Array::from_elem((m, n), f32::MAX);
+        let mut dtw = Array::from_elem((m, n), f64::MAX);
     
         dtw[[0,0]] = 0.;
     
@@ -40,14 +40,14 @@ fn rust_dtw(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
             for ti in lower_bound as usize..upper_bound as usize {
                 let cost = distance_fn(&s[si - 1], &t[ti - 1]);
                 dtw[[si, ti]] = cost
-                    + f32::min(
-                        f32::min(dtw[[si-1, ti]], dtw[[si, ti-1]]),
+                    + f64::min(
+                        f64::min(dtw[[si-1, ti]], dtw[[si, ti-1]]),
                         dtw[[si-1, ti-1]],
                     );
             }
         }
         if distance_mode.eq("euclidean"){
-            f32::sqrt(dtw[[n-1, m-1]])
+            f64::sqrt(dtw[[n-1, m-1]])
         }else{
             dtw[[n-1, m-1]]
         }
@@ -56,22 +56,22 @@ fn rust_dtw(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "dtw_connectome")]
     fn dtw_connnectome_py<'py>(
         py: Python<'py>,
-        connectome: PyReadonlyArray2<'_, f32>,
+        connectome: PyReadonlyArray2<'_, f64>,
         window: i32,
         vectorize: bool,
         distance_mode: String
-    )-> PyResult<&'py PyArray1<f32>>{
-        let result = dtw_connectome(connectome.as_array().view(), &window, select_distance(&distance_mode).unwrap(), &distance_mode);
+    )-> PyResult<&'py PyArray1<f64>>{
+        let result = dtw_connectome(connectome.as_array().view(), &window,select_distance(&distance_mode).unwrap(), &distance_mode);
         Ok(result.into_pyarray(py))
     }
 
     pub fn dtw_connectome(
-        connectome: ArrayView2<f32>,
+        connectome: ArrayView2<f64>,
         window: &i32,
-        distance_fn: fn(&f32, &f32) -> f32,
+        distance_fn: fn(&f64, &f64) -> f64,
         distance_mode: &String
-    ) -> Vec<f32> {
-        let mut result: Vec<f32> = vec![];
+    ) -> Vec<f64> {
+        let mut result: Vec<f64> = vec![];
         for i in 0..connectome.shape()[0]{ 
             for j in 0..i + 1 {
                 result.push(dtw(
@@ -89,30 +89,53 @@ fn rust_dtw(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     // #[pyfn(m, "dtw_connectomes")]
     // fn dtw_connnectomes_py<'py>(
     //     py: Python<'py>,
-    //     connectomes: PyArray3<f32>,
+    //     connectomes: PyArray3<f64>,
     //     window: i32,
     //     vectorize: bool,
     //     distance_mode: String
-    // )-> PyResult<&'py PyArray2<f32>>{
+    // )-> PyResult<&'py PyArray2<f64>>{
     //     let result = dtw_connectomes(connectomes.as_array().to_owned(), &window, select_distance(&distance_mode).unwrap(), &distance_mode);
     //     Ok(result.into_pyarray(py))
     // }
     
     pub fn dtw_connectomes(
-        connectomes: Array3<f32>,
+        connectomes: Array3<f64>,
         window: &i32,
-        distance_fn: fn(&f32, &f32) -> f32,
+        vectorize: bool,
+        distance_fn: fn(&f64, &f64) -> f64,
         distance_mode: &String
-    ) -> Vec<Vec<f32>> {
-        connectomes.axis_iter(Axis(0))
-            .into_par_iter()
-            .map(|connectome| dtw_connectome(connectome, window, distance_fn, distance_mode))
-            .collect()
+    ) -> ArrayD<f64>{
+
+        //This is not the fastest way and is not recommended by the docs
+        let result: Vec<Vec<f64>> = connectomes.axis_chunks_iter(Axis(0), connectomes.shape()[1])
+        .into_par_iter()
+        .map(|connectome| dtw_connectome(connectome, window, distance_fn, distance_mode)).collect();
+
+        // if vectorize{
+        //     Array2::from_shape_vec((dim, dim), result).unwrap()
+        // }else{
+        //     //convert to each vector (lower triangular) into symmetric matrix
+                //Fill Array2 from zeros and then fill lower triangular and + A.T
+        // }
+        
     }
 
-    pub fn select_distance(mode: &str) -> Result<fn(&f32, &f32) -> f32, Box<dyn Error>>{
+    pub fn vec_to_sym_mat(vec: Array1<f64>, dim: usize){
+        //TODO implement function which converts lower triangular matrix to sym
+        let mut full: Array2<f64> = Array2::zeros((dim, dim));
+        
+        for i  in 0..full.shape()[0]{
+            for j in 0..i + 1{
+                full[[i,j]] = vec[((i - 1) * i / 2) + j];
+            }
+        }
+
+        full = full + full.t()
+    }
+
+    pub fn select_distance(mode: &str) -> Result<fn(&f64, &f64) -> f64, Box<dyn Error>>{
         match mode {
-            "manhattan" => Ok(|a, b| f32::abs(a - b)),
+            "manhattan" => Ok(|a, b| f64::abs(a - b)),
             "euclidean" => Ok(|a, b| (a - b) * (a - b)),
             _ => Err("Please provide a valid distance metric.".into()),
         }
@@ -129,7 +152,7 @@ fn rust_dtw(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
 //     pub window: i32,
 //     pub vectorize: bool,
 //     pub distance_mode: String,
-//     pub distance_fn: fn(&f32, &f32) -> f32
+//     pub distance_fn: fn(&f64, &f64) -> f64
 // }
 
 
@@ -140,8 +163,8 @@ fn rust_dtw(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
 //     use super::*;
 //     #[test]
 //     fn dtw_test(){
-//         let s1:Array1<f32>  = array![0., 0., 1., 2., 1., 0., 1., 0., 0.];
-//         let s2:Array1<f32>  = array![0., 1., 2., 0., 0., 0., 0., 0., 0.];
+//         let s1:Array1<f64>  = array![0., 0., 1., 2., 1., 0., 1., 0., 0.];
+//         let s2:Array1<f64>  = array![0., 1., 2., 0., 0., 0., 0., 0., 0.];
 
 //         let result = dtw(s1.view(), s2.view(), &50, |a, b| (a - b) * (a - b), &String::from("euclidean"));
 //         assert_eq!(result, 1.4142135623730951)
